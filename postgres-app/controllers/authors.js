c9e871ceb5d8563c4e698e3abc7ec7f30bb2f7a7
Marko Hakkarainen <hakkis68@gmail.com>
2024-08-02 12:35:08 +0300

    13b

---
+++ postgres-app/controllers/authors.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models");
+
+const { sequelize } = require("../util/db");
+const { QueryTypes } = require("sequelize");
+
+router.get("/", async (req, res) => {
+  const blogs = await sequelize.query(
+    "SELECT author, count(*) AS blogs, sum(likes) AS likes from blogs group by author order by 3 DESC",
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+  res.json(blogs);
+});
+
+/*
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll({
+    attributes: {
+      include: [
+        [sequelize.fn("COUNT", sequelize.col("*")), "blogs"],
+        [sequelize.fn("SUM", sequelize.col("likes")), "likes"],
+      ],
+      exclude: ["userId", "id", "likes", "url", "title"],
+    },
+    group: ["author"],
+    order: [["likes", "DESC"]],
+  });
+  res.json(blogs);
+});
+*/
+module.exports = router;
