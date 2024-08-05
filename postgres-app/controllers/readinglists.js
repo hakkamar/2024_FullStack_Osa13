917526376e23f9a2e72b3f69cc70a4ef2d2e7a9b
Marko Hakkarainen <hakkis68@gmail.com>
2024-08-05 15:17:06 +0300

    13.22

--- postgres-app/controllers/readinglists.js
+++ postgres-app/controllers/readinglists.js
@@ -1,19 +1,12 @@
 const router = require("express").Router();
 
-const { Readinglist, Membership } = require("../models");
+const { Readinglist, Membership, User } = require("../models");
+
+const middleware = require("../util/middleware");
 
 router.post("/", async (req, res) => {
-  console.log("******************************");
-  console.log("readinglist POST");
-
   const { blog_id, user_id } = req.body;
-
-  console.log("blog_id", blog_id);
-  console.log("user_id", user_id);
-
   const readinglist = await Readinglist.create({ read: false });
-
-  console.log("readinglistId", readinglist.id);
 
   const createdReadings = await Membership.create({
     blogId: blog_id,
@@ -21,11 +14,30 @@ router.post("/", async (req, res) => {
     readinglistId: readinglist.id,
   });
 
-  console.log("createdReadings", createdReadings);
-
-  console.log("******************************");
-
   res.json(createdReadings);
+});
+
+router.put("/:id", middleware.tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const readinglist = await Readinglist.findByPk(req.params.id);
+
+  if (readinglist) {
+    const membership = await Membership.findOne({
+      where: { readinglistId: req.params.id },
+    });
+
+    if (user.id === membership.userId) {
+      // Varsinainen temppu mitä tultiin tekemään, eli read:in vaihto true/false mitä on pyydetty
+      readinglist.read = req.body.read;
+      await readinglist.save();
+
+      res.json(readinglist);
+    } else {
+      res.status(401).json({ error: "only own readinglist can be changed" });
+    }
+  }
+
+  res.status(204).end();
 });
 
 module.exports = router;
